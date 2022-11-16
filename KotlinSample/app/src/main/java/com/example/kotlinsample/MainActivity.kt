ea7ac8e355f1fe0033194fb770f2e3e8dfb10f5e
LeoAndo <reo.ando@sun-asterisk.com>
2022-11-16 13:20:43 +0900

    Kotlin Projectへの水平展開

--- KotlinSample/app/src/main/java/com/example/kotlinsample/MainActivity.kt
+++ KotlinSample/app/src/main/java/com/example/kotlinsample/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.kotlinsample
 
-import android.app.LocaleManager
 import android.os.Bundle
-import android.os.LocaleList
 import android.util.Log
-import android.widget.Button
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import java.util.*
 
 
@@ -13,43 +13,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         // 1. Inside an activity, in-app language picker gets an input locale "xx-YY"
         // 2. App calls the API to set its locale
-        val localeManager: LocaleManager = getSystemService(LocaleManager::class.java)
-
         val locales = arrayOf(
-            Locale.forLanguageTag("ja"), // Priority Max
-            Locale.forLanguageTag("ak"), // (not support locale)
+            Locale.forLanguageTag("ja"),  // Priority Max
+            Locale.forLanguageTag("ak"),  // (not support locale)
             Locale.forLanguageTag("zh"),
-            Locale.forLanguageTag("ko"),
+            Locale.forLanguageTag("ko")
         )
-
-        findViewById<Button>(R.id.buttonShuffle).setOnClickListener {
-            Log.d(TAG, "before locales: ${locales.map { it.displayName }}")
+        findViewById<View>(R.id.buttonShuffle).setOnClickListener {
+            Log.d(
+                TAG,
+                "before locales: " + locales[0].displayName
+            )
             locales.shuffle()
-            Log.d(TAG, "after shuffled locales: ${locales.map { it.displayName }}")
+            Log.d(
+                TAG,
+                "after shuffled locales:" + locales[0].displayName
+            )
         }
-
-        findViewById<Button>(R.id.buttonSet).setOnClickListener {
-            localeManager.applicationLocales = LocaleList(*locales)
-            // 3. The system updates the locale and restarts the app, including any configuration updates
-            // 4. The app is now displayed in "xx-YY" language
+        findViewById<View>(R.id.buttonSet).setOnClickListener {
+            AppCompatDelegate.setApplicationLocales(
+                LocaleListCompat.forLanguageTags(locales[0].language)
+            )
         }
-
-        findViewById<Button>(R.id.buttonReset).setOnClickListener {
+        findViewById<View>(R.id.buttonReset).setOnClickListener {
             // Pass a LocaleList#getEmptyLocaleList() to reset to the system locale.
-            localeManager.applicationLocales = LocaleList.getEmptyLocaleList()
+            AppCompatDelegate.setApplicationLocales(LocaleListCompat.getEmptyLocaleList())
         }
-
-        findViewById<Button>(R.id.buttonGet).setOnClickListener {
+        findViewById<View>(R.id.buttonGet).setOnClickListener {
             // 1. App calls the API to get the preferred locale
-            val currentAppLocales: LocaleList = localeManager.applicationLocales
+            val currentAppLocales = AppCompatDelegate.getApplicationLocales()
             Log.d(
                 TAG,
                 "onCreate: currentAppLocales toLanguageTags: " + currentAppLocales.toLanguageTags()
             )
-            // 2. App uses the returned LocaleList to display languages to the user
         }
     }
 
